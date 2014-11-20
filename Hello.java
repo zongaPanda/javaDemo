9ac16c0db25f06b6c780197a81b2f0865cbaf89f
RexZhong <tju_zhong@163.com>
2014-11-20 21:45:50 +0800

    Test successfully on connecting to Github.

--- Hello.java
+++ Hello.java
@@ -1,3 +1,6 @@
-public static void main(String[] args) {
-  System.out.print("Hello Rex.");
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello Rex.");
+    }
 }
